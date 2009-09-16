36190add8e9dd07938a31525aa13e7e3bc4a9665
Tangerine Cat <skljuchkovskij@localhost.(none)>
2009-09-16 17:35:35 +0600

    TESTING pony send

---
+++ dummy.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'pony'
+
+def test_pony
+  Pony.mail(:to => 'kaineer@gmail.com', :via => :smtp, :smtp => {
+              :host   => 'smtp.gmail.com',
+              :port   => '587',
+              :tls    => true,
+              :user   => 'kaineer',
+              :pass   => 'herahbless',
+              :auth   => :plain # :plain, :login, :cram_md5, no auth by default
+              :domain => "localhost.localdomain" # the HELO domain provided by the client to the server
+            })
+end
