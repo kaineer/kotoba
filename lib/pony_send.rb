7701fe38bcbd08e9c294c62c9f5b0c02414b1a48
Tangerine Cat <skljuchkovskij@localhost.(none)>
2009-09-17 10:54:31 +0600

    Added lib/*.rb

---
+++ lib/pony_send.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'pony'
+require './smtp_tls'
+
+def send_pony( to, subj, body )
+  Pony.mail(
+            :to => to,
+            :from => "kaineer@gmail.com",
+            :subject => subj,
+            :body => body,
+            :via => "smtp",
+            :smtp =>
+            {
+              :address => "smtp.gmail.com",
+              :port => 587,
+              :authentication => :plain,
+              :user_name => "kaineer@gmail.com",
+              :password => 'kerahbless'
+            } )
+end
