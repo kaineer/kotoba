93e47469c7be070c4e4ea2577fdc1b9d4d14d88c
Tangerine Cat <skljuchkovskij@localhost.(none)>
2009-09-17 10:22:44 +0600

    FIX? changed test_pony

--- dummy.rb
+++ dummy.rb
@@ -3,12 +3,12 @@ require 'pony'
 
 def test_pony
   Pony.mail(:to => 'kaineer@gmail.com', :via => :smtp, :smtp => {
-              :host   => 'smtp.gmail.com',
-              :port   => '587',
-              :tls    => true,
-              :user   => 'kaineer',
-              :pass   => 'kerahbless',
-              :auth   => :plain, # :plain, :login, :cram_md5, no auth by default
-              :domain => "localhost.localdomain" # the HELO domain provided by the client to the server
+              :host     => 'smtp.gmail.com',
+              :port     => '587',
+              :tls      => true,
+              :user     => 'kaineer@gmail.com',
+              :password => 'kerahbless',
+              :auth     => :plain, # :plain, :login, :cram_md5, no auth by default
+              :domain   => "localhost.localdomain" # the HELO domain provided by the client to the server
             })
 end
